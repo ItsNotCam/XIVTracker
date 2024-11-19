2b8abb1d1c36a07c15cca881e1594e2f0285f9fa
ItsNotCam <camallanyoung@gmail.com>
2024-11-19 03:44:44 -0500

    feat: implement XIVTrackerApp for managing Electron app lifecycle and WebSocket connections

---
+++ electron/app.ts
@@ -0,0 +1,89 @@
+import EventRegister from './libs/events/EventRegister';
+import { BrowserWindow, ipcMain } from 'electron'
+import EzWs from './libs/net/EzWs';
+import EzDb from './libs/db/EzDb';
+import ezRoute from '../electron/libs/net/EzRouter';
+import { DeserializedPacket } from '../electron/libs/net/EzTypes.d';
+
+export default class XIVTrackerApp {
+	private win: BrowserWindow;
+	private wsClient: EzWs;
+	private db: EzDb;
+	private eventRegister: EventRegister | null = null;
+
+	constructor(win: BrowserWindow) {
+		this.win = win;
+
+		try {
+			this.wsClient = new EzWs(
+				50085, 
+				this.handleUnregisteredMessage,
+				this.handleWsConnected
+			);
+		} catch (e) {
+			throw e;
+		}
+		
+		this.db = new EzDb();
+		this.eventRegister = new EventRegister(this);
+	}
+
+	public async init() {
+		await this.db.init();
+		this.wsClient.connect();
+
+		if(this.eventRegister) {
+			this.eventRegister.init();
+		}
+		
+		this.initWindowControls();
+	}
+	
+	public getDB = () => this.db;
+	public getWindow = () => this.win;
+	public getIpcMain = () => ipcMain;
+	public GetWebSocketClient = () => this.wsClient;
+
+	public close() {
+		this.wsClient.close();
+		this.db.close();
+		this.eventRegister!.close();
+	}
+	
+	private handleUnregisteredMessage = (data: DeserializedPacket) => {
+		ezRoute(this.win, data as DeserializedPacket);
+	}
+
+	private handleWsConnected = () => {
+		this.win.webContents.send("broadcast:tcp-connected", true);
+	}
+
+	private initWindowControls() {
+		ipcMain.on("exit", () => {
+			console.log("exit event received");
+			if (this.win) {
+				this.win.close();
+			} else {
+				console.error("BrowserWindow instance is not valid.");
+			}
+		});
+
+		ipcMain.on("minimize", () => {
+			console.log("minimize event received");
+			if (this.win) {
+				this.win.isMinimized() ? this.win.restore() : this.win.minimize();
+			} else {
+				console.error("BrowserWindow instance is not valid.");
+			}
+		});
+
+		ipcMain.on("maximize", () => {
+			console.log("maximize event received");
+			if (this.win) {
+				this.win.isMaximized() ? this.win.unmaximize() : this.win.maximize();
+			} else {
+				console.error("BrowserWindow instance is not valid.");
+			}
+		});
+	}
+}
--- electron/main.ts
+++ electron/main.ts
@@ -1,14 +1,8 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow } from 'electron'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
-
-import initHandlers, { initWindowControls } from './libs/events/handle';
-import ezRoute from './libs/net/EzRouter';
-import { DeserializedPacket } from './@types/EzNet';
-import EzWs from './libs/net/EzWs';
-
 import UpdateTCData from "../electron/data/updateTCData.mjs";
-import EzDb from '../electron/libs/db/EzDb';
+import XIVTrackerApp from './app';
 
 // const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -22,8 +16,7 @@ export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
 
 let win: BrowserWindow | null;
-let WebSocketClient: EzWs | null;
-let db: EzDb | null;
+let App: XIVTrackerApp | null;
 
 async function createWindow() {
 	win = new BrowserWindow({
@@ -41,22 +34,6 @@ async function createWindow() {
 	} else {
 		// win.loadFile('dist/index.html')
 		win.loadFile(path.join(RENDERER_DIST, 'index.html'))
-	}
-
-	initWindowControls(ipcMain!, win!);
-	initNetworking(win!);
-	initHandlers(win!, ipcMain, WebSocketClient!);
-}
-
-const initNetworking = (win: BrowserWindow) => {
-	try {
-		WebSocketClient = new EzWs(50085, (data: DeserializedPacket) => {
-			ezRoute(win, data as DeserializedPacket);
-		}, (connected: boolean) => {
-			win.webContents.send("broadcast:tcp-connected", connected);
-		}).connect();
-	} catch (e) {
-		console.error("error creating websocket client:\n", e);
 	}
 }
 
@@ -78,10 +55,10 @@ app.on('activate', () => {
 
 app.whenReady().then(async() => {
 	await UpdateTCData();
-	// db = await new EzDb().init();
 	await createWindow();
-});
 
-async function asyncSleep(ms: number) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
+	if(win) {
+		App = new XIVTrackerApp(win);
+		App.init();
+	}
+});
