cc52de54723d1f46ce647c6bebc8a49663f8ab2e
ItsNotCam <camallanyoung@gmail.com>
2024-12-06 02:02:42 -0500

    refactor: encapsulate JobState properties with private fields and accessors

--- electron/libs/JobState.ts
+++ electron/libs/JobState.ts
@@ -1,15 +1,25 @@
 export default class JobState {
-	public level: number;
-	public job_name: string;
-	public current_xp: number;
-	public max_xp: number;
+	private _level: number;
+	private _job_name: string;
+	private _current_xp: number;
+	private _max_xp: number;
 
 	constructor(level: number, jobName: string, currentXP: number, maxXP: number) {
-		this.level = level;
-		this.job_name = jobName;
-		this.current_xp = currentXP;
-		this.max_xp = maxXP;
+		this._level = level;
+		this._job_name = jobName;
+		this._current_xp = currentXP;
+		this._max_xp = maxXP;
 	}
+
+	public get level(): number { return this._level };
+	public get job_name(): string { return this._job_name };
+	public get current_xp(): number { return this._current_xp };
+	public get max_xp(): number { return this._max_xp };
+
+	public set level(level: number) { this._level = level };
+	public set job_name(jobName: string) { this._job_name = jobName };
+	public set current_xp(currentXP: number) { this._current_xp = currentXP };
+	public set max_xp(maxXP: number) { this._max_xp = maxXP };
 
 	static fromJson(json: string): JobState {
 		const state = JSON.parse(json);
